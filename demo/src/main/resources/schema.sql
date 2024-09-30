265e81fa39a4b7ed0bf30637aa4a41ea5da62fde
satoru <rhodeus208@gmail.com>
2024-09-30 17:34:15 +0900

    転校の情報を入れられるようにし始めている

--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -6,7 +6,12 @@ create table if not exists students(
     id int not null primary key auto_increment,
     code int  unique,
     name varchar(255) not null,
-    status varchar(50) not null,
-    school_id int not null,
-    foreign key(school_id)
-    references schools(id));
+    status varchar(50) not null);
+
+create table if not exists school_student(
+id int not null primary key auto_increment,
+school_id int not null,
+student_id int not null,
+created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+changed_at DATETIME  NOT NULL DEFAULT '9999-12-31 23:59:59' ON UPDATE CURRENT_TIMESTAMP ,
+)
